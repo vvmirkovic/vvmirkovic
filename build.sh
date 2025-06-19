d38f2a6770d0945a7f5464e306e2ff7e648c9eda
vvmirkovic <vvmirkovic@gmail.com>
2025-06-19 18:46:41 -0400

    initial website

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bind/zsh
+
+cp -r ../Rust_Platformer/assets/ ../vvmirkovic/apps/game/
+cp ../Rust_Platformer/out/giga_platformer.js ../vvmirkovic/apps/game/mygame.js
+cp ../Rust_Platformer/out/giga_platformer_bg.wasm ../vvmirkovic/apps/game/giga_platformer_bg.wasm
